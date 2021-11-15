cc949d98f9dae63f0d51046bebd3ad23a23bad3e
Srilekha Ganesan <srilekhaganesan@macbook-pro.lan>
2021-11-15 14:50:18 +0000

    multiple customer seat request

--- src/test.js
+++ src/test.js
@@ -8,9 +8,12 @@ test('request one seat when theatre empty ', () => {
     expect(main(numberOfseats)).toEqual('A1')       //act & assert
 })
 
-
-
 test('request two seats when theatre empty ', () => {
     const numberOfseats = ['2']                //arrange the data
     expect(main(numberOfseats)).toEqual('A1 A2')       //act & assert
+})
+
+test('multiple customer seat request  when theatre empty ', () => {
+    const numberOfseats = ['1','1']                //arrange the data
+    expect(main(numberOfseats)).toEqual(['A1','A1'])       //act & assert
 })
