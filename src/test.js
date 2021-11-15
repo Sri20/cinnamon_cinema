641285c5fc02d78af97053aeebb7a49f5d71b536
Srilekha Ganesan <srilekhaganesan@macbook-pro.lan>
2021-11-15 19:42:49 +0000

    theatre full,request more seat,seats not available

--- src/test.js
+++ src/test.js
@@ -33,7 +33,12 @@ test('multiple customer seat request  when theatre empty ,seats spreads multiple
     expect(main(numberOfseats)).toEqual(['A1,A2,A3','A4,A5,B1','B2','B3,B4,B5','C1,C2'])       //act & assert
 })
 
-test('multiple customer seat request  when theatre empty ,seats spreads multiple rows ', () => {
+test('full theatre ', () => {
     const numberOfseats = ['3','3','1','3','2','3']                //arrange the data
+    expect(main(numberOfseats)).toEqual(['A1,A2,A3','A4,A5,B1','B2','B3,B4,B5','C1,C2','C3,C4,C5'])       //act & assert
+})
+
+test('theatre full,seats not available ', () => {
+    const numberOfseats = ['3','3','1','3','2','3','1']                //arrange the data
     expect(main(numberOfseats)).toEqual(['A1,A2,A3','A4,A5,B1','B2','B3,B4,B5','C1,C2','C3,C4,C5'])       //act & assert
 })
