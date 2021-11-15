e53c4c4dc14de32fc9099c1977fa7d608f864274
Srilekha Ganesan <srilekhaganesan@macbook-pro.lan>
2021-11-15 14:23:18 +0000

    request one seat for empty theatre passed

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const main = numberOfseats => {
 console.log(numberOfseats)
-
-return ['A1']
+seatmap = {row:'A' ,seatno :['1']}
+return seatmap.row+seatmap.seatno
 }
 module.exports = main
--- src/test.js
+++ src/test.js
@@ -5,5 +5,5 @@ const main = require('./main.js')
 
 test('request one seat when theatre empty ', () => {
     const numberOfseats = ['1']                //arrange the data
-    expect(main(numberOfseats)).toEqual(['A1'])       //act & assert
+    expect(main(numberOfseats)).toEqual('A1')       //act & assert
 })
