4a4791182f01c09115df0c37daaa96455137712e
Srilekha Ganesan <srilekhaganesan@macbook-pro.lan>
2021-11-15 19:33:05 +0000

    multiple customer m ultiple across all the rows

--- src/test.js
+++ src/test.js
@@ -27,3 +27,8 @@ test('multiple customer seat request  when theatre empty ,seats spreads next row
     const numberOfseats = ['1','1','2','2']                //arrange the data
     expect(main(numberOfseats)).toEqual(['A1','A2','A3,A4','A5,B1'])       //act & assert
 })
+
+test('multiple customer seat request  when theatre empty ,seats spreads multiple rows ', () => {
+    const numberOfseats = ['3','3','1','3','2']                //arrange the data
+    expect(main(numberOfseats)).toEqual(['A1,A2,A3','A4,A5,B1','B2','B3,B4,B5','C1,C2'])       //act & assert
+})
