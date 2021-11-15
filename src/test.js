50a90665b509422e77d61bbcf9f41ec5052ab734
Srilekha Ganesan <srilekhaganesan@macbook-pro.lan>
2021-11-15 14:25:03 +0000

    request two seats when theatre empty ,failed

--- src/test.js
+++ src/test.js
@@ -7,3 +7,10 @@ test('request one seat when theatre empty ', () => {
     const numberOfseats = ['1']                //arrange the data
     expect(main(numberOfseats)).toEqual('A1')       //act & assert
 })
+
+
+
+test('request two seats when theatre empty ', () => {
+    const numberOfseats = ['1']                //arrange the data
+    expect(main(numberOfseats)).toEqual('A1 A2')       //act & assert
+})
